972a06ff95821b87e55e843c60e9b13d747067e4
DusanDjordjic <dukidjordjic@gmail.com>
2021-12-15 21:38:30 +0100

    Core & Home: basic html structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './core/nav/nav.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomePageComponent } from './home/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
